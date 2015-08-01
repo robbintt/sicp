1322e4a1e93e27052425c65eedfbd57556366932
Trent Robbins <robbintt@gmail.com>
2015-08-01 16:04:43 -0700

    Updated notes to reflect final observations.

--- exercises/chapter1/1.3.scm
+++ exercises/chapter1/1.3.scm
@@ -8,31 +8,30 @@
 (define (square x) (* x x))
 (define (sum_of_squares x y) (+ (square x) (square y)))
 
+
 ;; how should we determine the two largest numbers
-;; first return the largest number, then compare the two smaller.
-;; if a > b and a > c return a, and the greater of b,c
-;; else if b > c return b, else return c, 
+;; this procedure isn't used. instead conditions are checked manually
 (define (yield_higher m n) (if (> m n) m n))
 
 ;; find the lowest of two numbers
 (define (yield_lowest m n) (if (< m n) m n))
 
-;; find the lowest of three numbers
+;; find the lowest of three numbers, this can be used in a comparison later.
 (define (lowest_of_three x y z) (cond ((= (yield_lowest x y) (yield_lowest x z)) x)
                                       ((= (yield_lowest y x) (yield_lowest y z)) y)
                                       ((= (yield_lowest z x) (yield_lowest z y)) z)))
 
 ;; I don't have the tools to yield two results, so I will need to bake
 ;; this into the final procedure that consumes 2 arguments.
-;; (define (highest_two a b c) (yield_higher a b)
+;; (define (highest_two a b c) ...)
 
 ;; now lets consume my procedures
+;; 
 (define (f a b c) (sum_of_squares (cond ((= (lowest_of_three a b c) a) b)
                                         ((= (lowest_of_three a b c) b) c)
                                         ((= (lowest_of_three a b c) c) a))
                                   (cond ((= (lowest_of_three a b c) a) c)
                                         ((= (lowest_of_three a b c) b) a)
                                         ((= (lowest_of_three a b c) c) b))))
-(yield_lowest 2 3)
 (f 1 1 12)
 
