6930bd1cd1cc20cf0a63b9f4d01049f2d18d26f4
Trent Robbins <robbintt@gmail.com>
2015-08-01 17:12:09 -0700

    Successfully determined that mit-scheme is an applicative-order interpreter.

--- exercises/chapter1/exercises/1.5.scm
+++ exercises/chapter1/exercises/1.5.scm
@@ -6,19 +6,33 @@
 
 ;; Ben's application to determine which one the interpreter is using:
 
-; this self-definition is murky TAR 080115.1629
+; here we are defining p as its own evaluated result
 (define (p) (p))
-
 
 (define (test x y)
   (if (= x 0)
     0
     y))
 
-; evaluate
+; see what happens with definite numbers... result evaluates correctly.
+(test 0 5)
+
+; on evaluation the repl hangs.
 (test 0 (p))
+;;; CONCLUSION: mit-scheme uses applicative-order interpretation.
 
-;; What behavior do we get for applicative-order evaluation interpreters?
-
+; see if anything will evaluate after the hang. Probably not.
+(test 0 5)
 
 ;; What behavior do we get for normal-order evaluation interpreters?
+;;
+;; Normal order will substitute 0 and (p) into test, where it is evaluated
+;; and 0 is returned, because (= 0 0) yields 0 in this case.
+;; y isn't tested which is the (p) component.
+
+
+;; What behavior do we get for applicative-order evaluation interpreters?
+;;
+;; Applicative order will evaluate the arguments to test, then apply them.
+;; This means it will never apply test because it will evaluate (p) forever.
+
