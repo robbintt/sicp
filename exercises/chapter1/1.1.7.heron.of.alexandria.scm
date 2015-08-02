6174e2e8a2ed5cde87027cace1b3ae300afa7c5e
Trent Robbins <robbintt@gmail.com>
2015-08-01 17:50:08 -0700

    Added the heron of alexandria square root algorithm.

---
+++ exercises/chapter1/1.1.7.heron.of.alexandria.scm
@@ -0,0 +1,30 @@
+;;; special case of newtons sqrt algorithm method called by heron of alexandria
+
+;; note: radicand is the number you are finding the square root of.
+
+; square a number
+(define (square x) (* x x))
+
+; average two numbers
+(define (average x y) (/ (+ x y) 2))
+
+; average the guess with x/guess to improve the guess
+(define (improve guess radicand)
+  (average guess (/ radicand guess)))
+
+; determine when a new guess is close enough to the previous guess
+(define (good-enough? guess radicand) 
+  (< (abs (- (square guess) radicand)) 0.001))
+
+; main method, use the control flow
+(define (sqrt-iter guess radicand)
+  (if (good-enough? guess radicand)
+    guess
+    (sqrt-iter (improve guess radicand)
+               radicand)))
+
+; Give it something to chew on, set the first guess to 1.0.
+(define (sqrt x) 
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
