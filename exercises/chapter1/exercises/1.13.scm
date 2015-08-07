2c8244fdf9bc888857ef91b1cf24c133ea7f7048
Trent Robbins <robbintt@gmail.com>
2015-08-07 12:27:40 -0700

    Proving this was much easier using the hint and not worrying about where psi comes from.

--- exercises/chapter1/exercises/1.13.scm
+++ exercises/chapter1/exercises/1.13.scm
@@ -1,7 +1,14 @@
 ;;; Prove Fib(n) is the closest integer to phi^n / sqrt(5)
-;;;
-;;; 
+;;; I have no idea how the psi definition is arrived at, but I can use
+;;; induction to just do it based on my functions below.
 
+;;;
+;;; DEFINITIONS: Below are definitions used to perform math.
+;;;
+;;; NEED: an approximation method
+;;; (approx x NUM)  Return x truncated to a certain number of digits.
+;;; We could also take 2 numbers and say if they are within +/-x of each other.
+;;; This seems easier at a glance
 
 ;;; square x
 (define (square x) (* x x))
@@ -28,5 +35,36 @@
           ((= 1 count) result)
           (else (**iter (* x result) (- count 1)))))
   (**iter x y))
+
+;;; linear iteration following a->a+b, b->a. SICP p.39.
+(define (fib n)
+  (fib-iter 1 0 n))
+(define (fib-iter a b count)
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
+;;; direct definition of phi and psi
+;;; still not sure how psi is relevant, gonna have to look that up.
+(define (phi)
+  (/ (+ 1 (sqrt 5)) 2))
+(define (psi)
+  (/ (- 1 (sqrt 5)) 2))
+
+
+;;;
+;;;  1.13 Prove that Fib(n) is the closest integer to (phi**n  / sqrt(5))
+;;;  I've written an iterative test structure a few times now.
+;;;
+
+(define (test-fib-phi n)
+  (define (test-iter-fp c)
+    (cond ((= c 0) #t)
+          (= (fib c) (/ (- (** (phi) c) (** (psi) c)) (sqrt 5))
+          (test-iter-fp (- c 1)))
+          (else #f)))
+  (test-iter-fp n))
+
+(test-fib-phi 50)
 
 
