9fb46a592fadafb1d9f11361027e793d2e2f7a77
Trent Robbins <robbintt@gmail.com>
2015-10-16 14:02:31 -0700

    Finished 1.19, not bad\!

--- exercises/chapter1/1.19.scm
+++ exercises/chapter1/1.19.scm
@@ -1,5 +1,18 @@
 ; here is the code in exercise 1.19
 
+; the solution isn't hard, one hanging point is that the
+; first transformation is simply substituting T for a and b.
+; this means the second transformation is applying T into
+; itself. At first I did way too much algebra, trying to
+; perform what is essentially T'''.
+
+; these naturally fall out when putting T'' in the form of T.
+; p' = p^2+q^2
+; q' = q^2+2pq
+; note: this is done by extracting a and b, allowing you to
+; substitute these equations for p and q in T, to get T''
+
+(define (square a) (* a a))
 
 (define (fib n)
   (fib-iter 1 0 0 1 n))
@@ -9,8 +22,8 @@
         ((even? count)
          (fib-iter a
                    b
-                   ?? ; compute p'
-                   ?? ; compute q'
+                   (+ (square p) (square q)) ; compute p'
+                   (+ (square q) (* 2 (* p q))) ; compute q'
                    (/ count 2)))
         (else (fib-iter (+ (* b q) (* a q) (* a p))
                         (+ (* b p) (* a q))
@@ -18,3 +31,15 @@
                         q
                         (- count 1)))))
 
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 7)
+(fib 8)
+(fib 9)
+(fib 10)
+(fib 20)
+(fib 100)
