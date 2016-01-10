373f894098fda8a0a2457ec2a3188ec6f2447564
Trent Robbins <robbintt@gmail.com>
2016-01-10 17:00:58 -0500

    Completed 1.33

--- exercises/chapter1/1.33.scm
+++ exercises/chapter1/1.33.scm
@@ -5,8 +5,44 @@
 ;;;   filter methods take one value to test
 ;;;   filter methods return true or false
 
+;;;
+;;; PRIME TEST
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m)) m))
+         (else
+           (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n a)
+    (= (expmod a n n) a))
+
+(define (fast-prime? n times)
+  (cond ((= times 1) true)
+        ((fermat-test n times) (fast-prime? n (- times 1)))
+        (else false)))
+
+;;; USE AS FILTER FOR filtered-accumulate
+(define (comprehensive-prime? n) (fast-prime? n (- n 1)))
+;;; 
+;;; END PRIME TEST
+
+
+;;;
+;;; GCD TEST
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+(define (gcd-test? a b)
+  (if ((= (gcd a b) 1) true) false))
+
+;;;
+;;; END GCD TEST
+
 ;;; a filter with no constraints
-(define (empty-filter x) #t)
+(define (empty-filter x) true)
 
 (define (filtered-accumulate combiner null-value filter-method term a next b)
   (cond ((> a b) null-value)
@@ -29,15 +65,39 @@
 
 (define (factorial x)
   (product identity 1 inc x))
-
 (factorial 3)
-(factorial 4)
-(factorial 5)
 
 (define (sigma x)
   (sum identity 1 inc x))
-
 (sigma 3)
-(sigma 4)
-(sigma 5)
+
+;; 1.33a
+(define (sum-over-squares-of-primes x)
+  (filtered-accumulate + 0 comprehensive-prime? square 1 inc x))
+
+(sum-over-squares-of-primes 1)
+(sum-over-squares-of-primes 2)
+(sum-over-squares-of-primes 3)
+(sum-over-squares-of-primes 4)
+(sum-over-squares-of-primes 5)
+
+;; 1.33b
+(define (gcd-test-maker a)
+  (define (special-gcd-test? b)
+    (if (= (gcd a b) 1) true false))
+  special-gcd-test?)
+
+(define (special-product x)
+  (define (relative-prime-product n)
+    (filtered-accumulate * 1 (gcd-test-maker n) identity 1 inc n))
+  (relative-prime-product x))
+
+(special-product 1)
+(special-product 2)
+(special-product 3)
+(special-product 4)
+(special-product 5)
+(special-product 6)
+(special-product 7)
+
 
