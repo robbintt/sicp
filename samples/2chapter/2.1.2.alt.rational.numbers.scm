185b74e12dcbe9f954d31163c27034c34f4592ba
Trent Robbins <robbintt@gmail.com>
2017-10-01 12:02:20 -0700

    Replaced 2.1.1 with 2.1.2 (reduction in the selectors instead of constructor).

---
+++ samples/2chapter/2.1.2.alt.rational.numbers.scm
@@ -0,0 +1,68 @@
+; 2.1.2 Notes
+;
+; Alternately, we can perform rational number (fraction) reduction to lowest terms
+; whenever we access the parts of the rational number, rather than during construction
+; in other words, reduction can happen in the selectors instead of constructor
+
+
+; constructor
+; (make-rat n d) ; returns the rational number with the numerator integer n and denominator integer d
+
+; selector
+; (numer x) ; returns numerator of rational number x
+; (denom x) ; returns denominator of rational number x
+
+(define (make-rat n d) (cons n d))
+(define (numer x) 
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (car x) g)))
+(define (denom x) 
+  (let ((g (gcd (car x) (cdr x))))
+    (/ (cdr x) g)))
+
+; we can also define these strictly as aliases
+; this is more efficient because they are aliases instead of calling cons, car, cdr
+; but worse for debugging because there are way more calls to cons, car, cdr and 
+; you will have a hard time finding the cons, car, cdr relevant to your issue
+; (define make-rat cons)
+; (define numer car)
+; (define denom cdr)
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+  
+
+
+;; procedures from 2.1.1
+;;
+
+; add two rational numbers: add-rat
+(define (add-rat x y)
+  (make-rat (+ (* (numer y) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+; subtract two rational numbers: sub-rat
+
+; multipy two rational numbers: mul-rat
+
+; divide two rational numbers: div-rat
+
+; test equality of two rational numbers: equal-rat?
+
+;; tests
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+;(print-rat (mult-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
+
+(define two-thirds (make-rat 6 9))
+
+;; test that this is reduced
+(print-rat two-thirds)
