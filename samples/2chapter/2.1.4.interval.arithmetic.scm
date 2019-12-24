21a4f0d94f3575db87333d7656f7f3eec0d3352a
Trent Robbins <robbintt@gmail.com>
2019-12-24 12:14:21 -0800

    Work through interval math principles to setup for exercises 2.7-2.11

---
+++ samples/2chapter/2.1.4.interval.arithmetic.scm
@@ -0,0 +1,25 @@
+;; interval arithmetic - combining intervals, e.g. 6.8+-10% ohms in parallel with 4.7+-5% ohms yields the range 2.58 to 2.97 ohms.
+;; in the example above, operating on the two intervals yields an interval result
+;; abstract object "interval": two endpoints. given the endpoints, construct the interval using `make-interval`
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+;; trent: i surmise lower-bound/upper-bound will be a function/mask like car/cdr implementation
+
+;; using min/max, get product of 2 intervals, which doesn't always cover the original interval range, e.g. (* (2,4), (4,8)) == (8, 32)
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+;; first interval x multiplied by the inverse of the second interval y
+;; i suppose it takes the form x/y == x*(1/y) 
+;; then applies (1/y) on an interval which is applied as (1/(y1, y2)) == (1/y1, 1/y2)
+;; finally it computes x * (1/y1, 1/y2) by substituting the interval for x, as follows: (x1, x2) * (1/y1, 1/y2)
+(define (div-interval x y)
+  (mul-interval x
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
